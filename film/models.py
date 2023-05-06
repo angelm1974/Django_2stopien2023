af048d99de2bb102e2885ab1b9ca4e48226098cf
angelm1974 <mariusz980@gmail.com>
2023-05-06 18:04:05 +0200

    srodek

--- film/models.py
+++ film/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Film(models.Model):
+    title = models.CharField(max_length=200)
+    year = models.IntegerField(default=0)
+    description = models.TextField()
+    poster = models.ImageField(upload_to='filmy/postery')
+    url = models.URLField(blank=True, null=True)
+    
+    def __str__(self):
+        return self.title + " (" + str(self.year) + ")"
--- film/views.py
+++ film/views.py
@@ -4,7 +4,8 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return render(request, 'home.html', {'name':'Janusz'})
+    wyszukiwanie = request.GET.get('szukajFilmu')
+    return render(request, 'home.html', {'name': wyszukiwanie})
 
 def o_mnie(request):
     return HttpResponse("<H1 style='color:red;'>To jest moja strona</H1>")
