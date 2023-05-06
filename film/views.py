74f0b3bd84ec2713737dc74fa9e4c80815a0961f
angelm1974 <mariusz980@gmail.com>
2023-05-06 18:47:04 +0200

    lista

--- film/views.py
+++ film/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Film
 # Create your views here.
 
 def home(request):
     wyszukiwanie = request.GET.get('szukajFilmu')
-    return render(request, 'home.html', {'name': wyszukiwanie})
+    moje_filmy = Film.objects.all()
+    return render(request, 'home.html', {'name': wyszukiwanie, 'filmy': moje_filmy})
 
 def o_mnie(request):
     return HttpResponse("<H1 style='color:red;'>To jest moja strona</H1>")
